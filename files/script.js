ea3dafff107e62a6a82a04f168caa5ff7523dda8
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2022-02-16 10:14:47 +0100

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,9 +1,14 @@
 const params = new URLSearchParams(window.location.search);
 const file = params.get("f");
 var msg = document.getElementById('msg')
+var bar = document.getElementById('bar')
+var title = document.getElementById('title')
 
 if (!file) {
-  msg.innerHTML = "Error: no file id provided"
+  msg.innerHTML = "Error: no file id provided";
+  msg.style.color = "Red";
+  bar.style.opacity = 0;
+  title.innerHTML = "File ID not found";
 }
 else{
   window.location = "http://qbifiles.000webhostapp.com/" + file;
