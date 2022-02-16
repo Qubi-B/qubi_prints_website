6e82ebc869b4ca9410b4c28d0d4c83d89c492d34
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2022-02-16 10:32:19 +0100

    check if file exists before redirect

--- files/script.js
+++ files/script.js
@@ -11,5 +11,23 @@ if (!file) {
   title.innerHTML = "File ID not found";
 }
 else{
-  window.location = "http://qbifiles.000webhostapp.com/" + file;
+  msg.innerHTML = "Checking file...";
+  msg.style.color = "White";
+
+  var url = "http://qbifiles.000webhostapp.com/" + file;
+  var img = new Image();
+  img.src = url;
+  img.onload = function()
+  {
+    msg.innerHTML = "You will be redirected soon...";
+    msg.style.color = "Green";
+    window.location = "http://qbifiles.000webhostapp.com/" + file;
+  }
+  img.onerror = function()
+  {
+    msg.innerHTML = "Error: selected file doesn't exist";
+    msg.style.color = "Red";
+    bar.style.opacity = 0;
+    title.innerHTML = "File doesn't exist";
+  }
 }
