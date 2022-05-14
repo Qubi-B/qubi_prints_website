11c3b0cd484cb6c9187f603ab1923e38a6e148a0
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2022-05-14 21:06:58 +0200

    Create img.js

---
+++ img.js
@@ -0,0 +1,55 @@
+bg = document.body.style.backgroundImage
+cr = document.getElementById("image")
+
+img = Math.floor(Math.random() * 8); //0 to 2
+
+if (img == 0) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611505908502-5b67e53e3a76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")'
+  cr.href = 'https://unsplash.com/photos/HsefvbLbNWc'
+  cr.innerHTML = 'Kadir Celep'
+}
+else if (img == 1) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1633899306328-c5e70574aaa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80")'
+  cr.href = 'https://unsplash.com/photos/Q1NS-nISNIw'
+  cr.innerHTML = 'Gavin Allanwood'
+}
+else if (img == 2) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611117775350-ac3950990985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80")'
+  cr.href = 'https://unsplash.com/photos/aCniNTiIFd8'
+  cr.innerHTML = 'Minku Kang'
+}
+else if (img == 3) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1642969164999-979483e21601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'
+  cr.href = 'https://unsplash.com/photos/NMCABEhN0RE'
+  cr.innerHTML = 'Osman Talha Dikyar'
+}
+else if (img == 4) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1615378809998-afc205e73bad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'
+  cr.href = 'https://unsplash.com/photos/7NvkRlTzLZE'
+  cr.innerHTML = 'Opt Lasers'
+}
+else if (img == 5) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1614086138082-8f9f4bed81e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'
+  cr.href = 'https://unsplash.com/photos/hfoIlAvHuPw'
+  cr.innerHTML = 'eMotion Tech'
+}
+else if (img == 6) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1532186773960-85649e5cb70b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80")'
+  cr.href = 'https://unsplash.com/photos/wSTCaQpiLtc'
+  cr.innerHTML = 'Louis Reed'
+}
+else if (img == 7) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1609862776364-897efc7dafdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")'
+  cr.href = 'https://unsplash.com/photos/Yc9ROfBvWEw'
+  cr.innerHTML = 'Andrea G'
+}
+else if (img == 8) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1597765654525-5cb60d312ef6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'
+  cr.href = 'https://unsplash.com/photos/1M7_12ynKG8'
+  cr.innerHTML = 'Tom Claes'
+}
+else if (img == 9) {
+  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1563520240533-66480a3916fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'
+  cr.href = 'https://unsplash.com/photos/p1m4B-lhS9Y'
+  cr.innerHTML = 'ZMorph All-In-One 3D Printers'
+}
