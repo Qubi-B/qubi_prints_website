5890e2c8b315139a4eaf640a5d258e585d3fe608
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2022-06-22 22:33:58 +0200

    Add files via upload

---
+++ orders/read.js
@@ -0,0 +1,16 @@
+name = document.getElementById('name');
+id = document.getElementById('ordid');
+date = document.getElementById('orddate');
+price = document.getElementById('price');
+
+let params = new URLSearchParams(location.search);
+orderid = params.get('i');
+
+
+document.getElementById('name').innerHTML = params.get('n');
+id.innerHTML = orderid;
+price.innerHTML = parseFloat(params.get('p')).toFixed(2);
+
+ordday = orderid.slice(2,4) + "." + orderid.slice(6,8) + ".20" + orderid.slice(4,6) + " " + orderid.slice(0, 2) + ":" + orderid.slice(8,10) + ":" + orderid.slice(10,12)
+
+date.innerHTML = ordday;
