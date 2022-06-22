371269173764cfd4a3d50ab0b44c9675a76a84de
Jakub Barszcz <61910780+Qubi-B@users.noreply.github.com>
2022-06-22 20:37:22 +0200

    Update img.js

--- img.js
+++ img.js
@@ -1,112 +1,111 @@
-bg = document.body.style.backgroundImage
+bg = document.getElementById("intro")
 cr = document.getElementById("image")
 
 
 img = Math.floor(Math.random() * 21);
 
 if (img == 0) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611505908502-5b67e53e3a76")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611505908502-5b67e53e3a76")'
   cr.href = 'https://unsplash.com/photos/HsefvbLbNWc'
   cr.innerHTML = 'Kadir Celep'
 }
 else if (img == 1) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1633899306328-c5e70574aaa2")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1633899306328-c5e70574aaa2")'
   cr.href = 'https://unsplash.com/photos/Q1NS-nISNIw'
   cr.innerHTML = 'Gavin Allanwood'
 }
 else if (img == 2) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611117775350-ac3950990985")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611117775350-ac3950990985")'
   cr.href = 'https://unsplash.com/photos/aCniNTiIFd8'
   cr.innerHTML = 'Minku Kang'
 }
 else if (img == 3) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1642969164999-979483e21601")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1642969164999-979483e21601")'
   cr.href = 'https://unsplash.com/photos/NMCABEhN0RE'
   cr.innerHTML = 'Osman Talha Dikyar'
 }
 else if (img == 4) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1615378809998-afc205e73bad")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1615378809998-afc205e73bad")'
   cr.href = 'https://unsplash.com/photos/7NvkRlTzLZE'
   cr.innerHTML = 'Opt Lasers'
 }
 else if (img == 5) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1614086138082-8f9f4bed81e4")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1614086138082-8f9f4bed81e4")'
   cr.href = 'https://unsplash.com/photos/hfoIlAvHuPw'
   cr.innerHTML = 'eMotion Tech'
 }
 else if (img == 6) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1532186773960-85649e5cb70b")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1532186773960-85649e5cb70b")'
   cr.href = 'https://unsplash.com/photos/wSTCaQpiLtc'
   cr.innerHTML = 'Louis Reed'
 }
 else if (img == 7) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1609862776364-897efc7dafdb")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1609862776364-897efc7dafdb")'
   cr.href = 'https://unsplash.com/photos/Yc9ROfBvWEw'
   cr.innerHTML = 'Andrea G'
 }
 else if (img == 8) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1597765654525-5cb60d312ef6")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1597765654525-5cb60d312ef6")'
   cr.href = 'https://unsplash.com/photos/1M7_12ynKG8'
   cr.innerHTML = 'Tom Claes'
 }
 else if (img == 9) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1563520240533-66480a3916fe")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1563520240533-66480a3916fe")'
   cr.href = 'https://unsplash.com/photos/p1m4B-lhS9Y'
   cr.innerHTML = 'ZMorph All-In-One 3D Printers'
 }
 else if (img == 10) {
-  document.body.style.backgroundImage = 'url("https://images.pexels.com/photos/8865186/pexels-photo-8865186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")'
+  bg.style.backgroundImage = 'url("https://images.pexels.com/photos/8865186/pexels-photo-8865186.jpeg")'
   cr.href = 'https://www.pexels.com/photo/black-and-yellow-camera-on-black-table-8865186/'
   cr.innerHTML = 'Daniel Smyth'
 }
 else if (img == 11) {
-  document.body.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2019/07/19/07/18/printer-4348150_960_720.jpg")'
+  bg.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2019/07/19/07/18/printer-4348150_960_720.jpg")'
   cr.href = 'https://pixabay.com/photos/printer-technology-3d-printer-4348150/'
   cr.innerHTML = 'ZMorph3D'
 }
 else if (img == 12) {
-  document.body.style.backgroundImage = 'url("https://images.pexels.com/photos/246753/pexels-photo-246753.jpeg")'
+  bg.style.backgroundImage = 'url("https://images.pexels.com/photos/246753/pexels-photo-246753.jpeg")'
   cr.href = 'https://www.pexels.com/photo/close-up-of-machine-part-246753/'
   cr.innerHTML = 'Anton Mislawsky'
 }
 else if (img == 13) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1636484229128-dbbf72bbe764")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1636484229128-dbbf72bbe764")'
   cr.href = 'https://unsplash.com/photos/vdxSOOZucTc'
   cr.innerHTML = 'Greg Rosenke'
 }
 else if (img == 14) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1549563316-5384a923453e")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1549563316-5384a923453e")'
   cr.href = 'https://unsplash.com/photos/nHRfTeqAxjs'
   cr.innerHTML = 'Rob Wingate'
 }
 else if (img == 15) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc")'
   cr.href = 'https://unsplash.com/photos/hoivM01c-vg'
   cr.innerHTML = 'thisisengineering'
 }
 else if (img == 16) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1580411737935-de5d0312fd68")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1580411737935-de5d0312fd68")'
   cr.href = 'https://unsplash.com/photos/TbHPvVcZ_f4'
   cr.innerHTML = 'Nabeel Hussain'
 }
 else if (img == 17) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1593106410288-caf65eca7c9d")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1593106410288-caf65eca7c9d")'
   cr.href = 'https://unsplash.com/photos/hkJNx0EDbjE'
   cr.innerHTML = 'Clayton Cardinalli'
 }
 else if (img == 18) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1633164227069-df58d5f183df")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1633164227069-df58d5f183df")'
   cr.href = 'https://unsplash.com/photos/hkJNx0EDbjE'
   cr.innerHTML = 'Mike U'
 }
 else if (img == 19) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1619204715997-1367fe5812f1")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1619204715997-1367fe5812f1")'
   cr.href = 'https://unsplash.com/photos/y4EkDeDEoSE'
   cr.innerHTML = 'Daniel Spase'
 }
 else if (img == 20) {
-  document.body.style.backgroundImage = 'url("https://images.unsplash.com/photo-1628755847299-20f509e81973")'
+  bg.style.backgroundImage = 'url("https://images.unsplash.com/photo-1628755847299-20f509e81973")'
   cr.href = 'https://unsplash.com/photos/gVb_xcGkNdQ'
   cr.innerHTML = 'Karim Elgamil'
 }
-
